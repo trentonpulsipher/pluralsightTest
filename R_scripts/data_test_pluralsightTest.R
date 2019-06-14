66eccf3a5aecd424b16ddbc6b5cba1c79ec488d0
trentonpulsipher <tcpulsipher@gmail.com>
2019-06-14 09:45:37 -0600

    data ready, attached instructions

--- R_scripts/data_test_pluralsightTest.R
+++ R_scripts/data_test_pluralsightTest.R
@@ -6,15 +6,26 @@
 library(DBI)
 library(RSQLite)
 library(tidyverse)
+library(lubridate)
 
 # connect the database created by running .sql file in TablePlus
 con <- DBI::dbConnect(RSQLite::SQLite(), "pluralsightTestDB.sqlite3")
 
 # read in data from the four database tables
-question_details <- DBI::dbReadTable(con, "question_details") %>% as_tibble()
-question_interactions <- DBI::dbReadTable(con, "question_interactions") %>% as_tibble()
-user_assessment_sessions <- DBI::dbReadTable(con, "user_assessment_sessions") %>% as_tibble()
-user_interactions <- DBI::dbReadTable(con, "user_interactions") %>% as_tibble()
+question_details <- DBI::dbReadTable(con, "question_details") %>% 
+  as_tibble()
+
+question_interactions <- DBI::dbReadTable(con, "question_interactions") %>% 
+  as_tibble() %>%
+  mutate(date_created = ymd_hms(date_created))
+
+user_assessment_sessions <- DBI::dbReadTable(con, "user_assessment_sessions") %>% 
+  as_tibble() %>%
+  mutate(date_created = ymd_hms(date_created))
+
+user_interactions <- DBI::dbReadTable(con, "user_interactions") %>% 
+  as_tibble() %>%
+  mutate(date_created = ymd_hms(date_created))
 
 
 
