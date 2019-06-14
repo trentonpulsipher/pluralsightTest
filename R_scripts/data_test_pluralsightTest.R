647c31d5333a57d8b13c2de38cfbdaf01348ee4e
trentonpulsipher <tcpulsipher@gmail.com>
2019-06-14 14:35:10 -0600

    join the four datasets together

--- R_scripts/data_test_pluralsightTest.R
+++ R_scripts/data_test_pluralsightTest.R
@@ -31,5 +31,31 @@ user_interactions <- DBI::dbReadTable(con, "user_interactions") %>%
   as_tibble() %>%
   mutate(date_created = ymd_hms(date_created))
 
+## Join the data together
+d <- user_assessment_sessions %>%
+  left_join(user_interactions, by = "user_assessment_session_id") %>%
+  rename(
+    ranking_overall = ranking.x,
+    rd_overall = rd.x,
+    display_score_overall = display_score.x,
+    percentile_overall = percentile.x,
+    date_created_overall = date_created.x,
+    ranking_interaction = ranking.y,
+    rd_interaction = rd.y,
+    display_score_interaction = display_score.y,
+    percentile_interaction = percentile.y,
+    date_created_interation = date_created.y
+  ) %>%
+  left_join(question_interactions %>%
+              left_join(question_details, by = "item_content_id") %>%
+              rename(
+                date_created = date_created.x,
+                date_created_question_details = date_created.y
+              )
+            , by = c("user_assessment_session_id", "user_interaction_id", "assessment_item_id", "assessment_id"))
 
-
+d %>% 
+  filter(
+    assessment_id == 31584,
+    user_id == 2512356
+  )
