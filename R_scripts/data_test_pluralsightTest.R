027934bf34befe96d8f372515488b20b4b7e0930
trentonpulsipher <tcpulsipher@gmail.com>
2019-06-14 09:05:19 -0600

    initial setup of data and scripts

---
+++ R_scripts/data_test_pluralsightTest.R
@@ -0,0 +1,20 @@
+
+
+# checked to make sure SQLite is loaded on my mac
+# downloaded TablePlus application to work with SQLite
+
+library(DBI)
+library(RSQLite)
+library(tidyverse)
+
+# connect the database created by running .sql file in TablePlus
+con <- DBI::dbConnect(RSQLite::SQLite(), "pluralsightTestDB.sqlite3")
+
+# read in data from the four database tables
+question_details <- DBI::dbReadTable(con, "question_details") %>% as_tibble()
+question_interactions <- DBI::dbReadTable(con, "question_interactions") %>% as_tibble()
+user_assessment_sessions <- DBI::dbReadTable(con, "user_assessment_sessions") %>% as_tibble()
+user_interactions <- DBI::dbReadTable(con, "user_interactions") %>% as_tibble()
+
+
+
